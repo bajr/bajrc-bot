0e65d04836d388166796f42a2e49063354ae07ea
Bradley Rasmussen <bajr@cat.pdx.edu>
2014-10-28 17:42:08 -0700

    Made local functions in irc.c static.

--- irc.c
+++ irc.c
@@ -3,6 +3,22 @@
 #include <string.h>
 #include <time.h>
 
+static int irc_parse_action(irc_t *irc);
+static int irc_leave_channel(irc_t *irc); 
+static int irc_log_message(irc_t *irc, const char *nick, const char* msg);
+static int irc_reply_message(irc_t *irc, char *nick, char* msg);
+static int irc_pong(int s, const char *data);
+static int irc_reg(int s, const char *nick, const char *username, const char *fullname);
+static int irc_join(int s, const char *data);
+static int irc_part(int s, const char *data);
+static int irc_nick(int s, const char *data);
+static int irc_quit(int s, const char *data);
+static int irc_topic(int s, const char *channel, const char *data);
+static int irc_action(int s, const char *channel, const char *data);
+static int irc_msg(int s, const char *channel, const char *data);
+
+
+
 int irc_connect(irc_t *irc, const char* server, const char* port) {
    if ( (irc->s =  get_socket(server, port)) < 0 ) {
       return -1;
@@ -21,7 +37,7 @@ int irc_join_channel(irc_t *irc, const char* channel) {
    return irc_join(irc->s, channel);
 }
 
-int irc_leave_channel(irc_t *irc) {
+static int irc_leave_channel(irc_t *irc) {
    return irc_part(irc->s, irc->channel);
 }
 
@@ -55,7 +71,7 @@ int irc_handle_data(irc_t *irc) {
    return 0;
 }
 
-int irc_parse_action(irc_t *irc) {
+static int irc_parse_action(irc_t *irc) {
    char irc_nick[128];
    char irc_msg[512];
 
@@ -120,7 +136,7 @@ int irc_set_output(irc_t *irc, const char* file) {
    return 0;
 }
 
-int irc_reply_message(irc_t *irc, char *irc_nick, char *msg) {
+static int irc_reply_message(irc_t *irc, char *irc_nick, char *msg) {
    // Checks if someone calls on the bot.
    if ( *msg != '!' )
       return 0;
@@ -181,7 +197,7 @@ int irc_reply_message(irc_t *irc, char *irc_nick, char *msg) {
    return 0;
 }
 
-int irc_log_message(irc_t *irc, const char* nick, const char* message) {
+static int irc_log_message(irc_t *irc, const char* nick, const char* message) {
    char timestring[128];
    time_t curtime;
    time(&curtime);
@@ -197,52 +213,51 @@ void irc_close(irc_t *irc) {
    fclose(irc->file);
 }
 
-
 // irc_pong: For answering pong requests...
-int irc_pong(int s, const char *data) {
+static int irc_pong(int s, const char *data) {
    return sck_sendf(s, "PONG :%s\r\n", data);
 }
 
 // irc_reg: For registering upon login
-int irc_reg(int s, const char *nick, const char *username, const char *fullname) {
+static int irc_reg(int s, const char *nick, const char *username, const char *fullname) {
    return sck_sendf(s, "NICK %s\r\nUSER %s localhost 0 :%s\r\n", nick, username, fullname);
 }
 
 // irc_join: For joining a channel
-int irc_join(int s, const char *data) {
+static int irc_join(int s, const char *data) {
    return sck_sendf(s, "JOIN %s\r\n", data);
 
 }
 
 // irc_part: For leaving a channel
-int irc_part(int s, const char *data) {
+static int irc_part(int s, const char *data) {
    return sck_sendf(s, "PART %s\r\n", data);
 
 }
 
 // irc_nick: For changing your nick
-int irc_nick(int s, const char *data) {
+static int irc_nick(int s, const char *data) {
    return sck_sendf(s, "NICK %s\r\n", data);
 
 }
 
 // irc_quit: For quitting IRC
-int irc_quit(int s, const char *data) {
+static int irc_quit(int s, const char *data) {
    return sck_sendf(s, "QUIT :%s\r\n", data);
 }
 
 // irc_topic: For setting or removing a topic
-int irc_topic(int s, const char *channel, const char *data) {
+static int irc_topic(int s, const char *channel, const char *data) {
    return sck_sendf(s, "TOPIC %s :%s\r\n", channel, data);
 }
 
 // irc_action: For executing an action (.e.g /me is hungry)
-int irc_action(int s, const char *channel, const char *data) {
+static int irc_action(int s, const char *channel, const char *data) {
    return sck_sendf(s, "PRIVMSG %s :\001ACTION %s\001\r\n", channel, data);
 }
 
 // irc_msg: For sending a channel message or a query
-int irc_msg(int s, const char *channel, const char *data) {
+static int irc_msg(int s, const char *channel, const char *data) {
    return sck_sendf(s, "PRIVMSG %s :%s\r\n", channel, data);
 }
 
